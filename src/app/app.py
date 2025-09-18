f8da200c9eb450b427b5d5c0ede0a7be0aadeb6d
deriancampos <71363391+DerianCampos@users.noreply.github.com>
2025-09-17 18:54:24 -0600

    feat: adding instance and config fastapi

---
+++ src/app/app.py
@@ -0,0 +1,40 @@
+import os
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from src.app.config.app_config import AppConfig
+from src.app.features.user_management.infrastructure.web.routes.user_routes import router as user_router
+
+ENV = os.getenv("APP_ENV", "local")
+
+config = AppConfig.instance()
+app_name = config.get_config("app.name")
+app_version = config.get_config("app.version")
+
+fastApiApp = FastAPI(title=app_name, root_path="/api", version=app_version)
+
+if ENV not in ("local", "container"):
+    fastApiApp.docs_url = None
+    fastApiApp.redoc_url = None
+    fastApiApp.openapi_url = None
+
+# --- CORS Origins from config ---
+origins = [
+    "http://localhost",  # Add your React app's URL here
+]
+
+fastApiApp.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
+
+
+@fastApiApp.get("/health")
+def get_health_check():
+    return "Ok"
+
+
+fastApiApp.include_router(user_router, prefix="/v1/user", tags=["Users"])
