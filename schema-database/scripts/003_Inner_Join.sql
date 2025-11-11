a8e1e50fcccba416158137f779b6c33bfcb0ce36
Yosser Quesada C. <yosserquesada@gmail.com>
2025-11-11 12:32:55 -0600

    Last Add

--- schema-database/scripts/003_Inner_Join.sql
+++ schema-database/scripts/003_Inner_Join.sql
@@ -1,6 +1,6 @@
 SELECT U.user_name,P.project_name,P.project_description,T.task_title, T.task_description, T.due_date, T.completed
-FROM task T
-INNER JOIN project P 
+FROM tasks T
+INNER JOIN projects P 
 ON P.id = T.project_id
 INNER JOIN users U
 ON U.id = T.user_id
